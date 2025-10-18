e3540e6b77a4f709a360b0e8d939399723abaf9a
asksakyrime <asksakyrime@gmail.com>
2025-10-18 07:29:16 +0000

    rjvbn

---
+++ sites/skuit/www/lab1/hello_php.php
@@ -0,0 +1,10 @@
+<?php
+$sum = 2006 + 7 -1;
+$fio = 'Дима' . 'Бугаев';
+$length = 1 * 2;
+$width = 11 * 7;
+$aquare = $length * $width;
+
+echo "Результат сложение трех чисел: " . $sum . "\n";
+echo "Меня зовут: " . $fio . "\n";
+echo $aquare ;
